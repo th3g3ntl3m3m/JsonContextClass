d11bf563b096e9a811b02d3379a7d21ee0a334a9
Semen Kaunov <gt0vip1jake@gmail.com>
2022-04-28 18:16:37 +0500

    add example

--- JsonContextClass/JsonContextClass.cpp
+++ JsonContextClass/JsonContextClass.cpp
@@ -11,12 +11,20 @@ int main()
 
 	Val.SetInt(10);
 	std::cout << "Int single Val " << Val.GetInt() << std::endl;
+	std::cout << Val.IsInt() << " "
+		<< Val.IsDouble() << " "
+		<< Val.IsBool() << " "
+		<< Val.IsTrue() << " "
+		<< Val.IsFalse() << " " << std::endl;
+
 
 	Val.SetDouble(.101);
 	std::cout << "Double single Val " << Val.GetDouble() << std::endl;
 
 	Val.SetBool(true);
 	std::cout << "Bool single Val " << Val.GetBool() << std::endl;
+
+
 
 	return 0;
 }
